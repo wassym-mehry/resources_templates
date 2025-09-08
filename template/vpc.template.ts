ef85106f0202e74979026de89ecb4be268766be7
wassym-mehry <wassim.mehri@fenleap.com>
2025-09-08 11:56:38 +0100

    Update vpc.template.ts

--- template/vpc.template.ts
+++ template/vpc.template.ts
@@ -103,7 +103,7 @@ locals {
 /* LOCALS_TF_END */
 
 /* MODULE_VPC_START */
-resource "aws_vpc" "{{name}}" {
+resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
@@ -113,57 +113,57 @@ resource "aws_vpc" "{{name}}" {
   })
 }
 
-resource "aws_internet_gateway" "{{name}}" {
-  vpc_id = aws_vpc.{{name}}.id
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
 
   tags = merge(var.tags, {
-    Name = "${var.name}-igw"
+    Name = "\${var.name}-igw"
   })
 }
 
-resource "aws_subnet" "{{name}}_public" {
+resource "aws_subnet" "public" {
   count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.{{name}}.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = var.public_subnets[count.index]
   availability_zone = var.azs[count.index]
 
   map_public_ip_on_launch = true
 
   tags = merge(var.tags, {
-    Name = "${var.name}-public-${var.azs[count.index]}"
+    Name = "\${var.name}-public-\${var.azs[count.index]}"
     Type = "Public"
   })
 }
 
-resource "aws_subnet" "{{name}}_private" {
+resource "aws_subnet" "private" {
   count             = length(var.private_subnets)
-  vpc_id            = aws_vpc.{{name}}.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_subnets[count.index]
   availability_zone = var.azs[count.index]
 
   tags = merge(var.tags, {
-    Name = "${var.name}-private-${var.azs[count.index]}"
+    Name = "\${var.name}-private-\${var.azs[count.index]}"
     Type = "Private"
   })
 }
 
-resource "aws_route_table" "{{name}}_public" {
-  vpc_id = aws_vpc.{{name}}.id
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.{{name}}.id
+    gateway_id = aws_internet_gateway.this.id
   }
 
   tags = merge(var.tags, {
-    Name = "${var.name}-public-rt"
+    Name = "\${var.name}-public-rt"
   })
 }
 
-resource "aws_route_table_association" "{{name}}_public" {
-  count          = length(aws_subnet.{{name}}_public)
-  subnet_id      = aws_subnet.{{name}}_public[count.index].id
-  route_table_id = aws_route_table.{{name}}_public.id
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
 
 variable "name" {
@@ -211,17 +211,17 @@ variable "tags" {
 
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.{{name}}.id
+  value       = aws_vpc.this.id
 }
 
 output "public_subnet_ids" {
   description = "List of IDs of public subnets"
-  value       = aws_subnet.{{name}}_public[*].id
+  value       = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
   description = "List of IDs of private subnets"
-  value       = aws_subnet.{{name}}_private[*].id
+  value       = aws_subnet.private[*].id
 }
 /* MODULE_VPC_END */
 `
